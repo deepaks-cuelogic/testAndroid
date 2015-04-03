c5a229233ca2e175ac8faf72059130ea1394704b
Deepak Sherveghar <deepak.sherveghar@cuelogic.co.in>
2015-04-03 18:02:48 +0530

    Added new method isThisRequired.

--- source/secondFile.java
+++ source/secondFile.java
@@ -1,5 +1,10 @@
 
 public static void main(String[] args)
 {
-    System.out.println("My world");
+    System.out.println("Test world");
+}
+
+void isThisRequired()
+{
+  System.out.println(is this required");
 }
